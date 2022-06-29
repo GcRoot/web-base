c0952b978f6ab3e21738e6c856ca2ed41ccfeaba
hanxi <>
2022-06-29 15:07:10 +0800

    feat: 工具包行为设计

--- scripts/bootstrap/packages.ts
+++ scripts/bootstrap/packages.ts
@@ -3,10 +3,16 @@
 import {Package} from './package'
 import { resolve } from 'path';
 
-
+/**
+ * 
+ * 行为边界问题：
+ * 1.安装后下次安装 缓存问
+ * 2.运行的时候，保证依赖安装完成，or 关联关系已生成好 
+*/
 export class Packages {
   packages: Array<Package>
   package: Package
+  marks: object  //行为边界控制状态
   constructor(packages:Array<Package>){
     // packages.filter()
     this.package = new Package('package.json', resolve(__dirname,'../../') )
--- scripts/bootstrap/projects.ts
+++ scripts/bootstrap/projects.ts
@@ -1,6 +1,7 @@
 // todo 项目整合工具
 import {Packages} from './packages'
 
-export function initProject() {
+export function initProject() : Packages{
+  // 去固定路径找子项目，然后返回自己能力的packages
   return new Packages()
 }
